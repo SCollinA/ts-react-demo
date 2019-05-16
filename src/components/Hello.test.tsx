a3773b50d9bf76e1ee02afe6925f8fb590dc2bce
Collin Argo <collinargo@gmail.com>
2019-05-16 14:50:54 -0400

    rewrite Hello component as stateful component

---
+++ src/components/Hello.test.tsx
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Hello from './Hello';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Hello name='TypeScript' />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Hello from './components/Hello';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Hello name='TypeScript' enthusiasmLevel={10} />,
+  document.getElementById('root') as HTMLElement
+  // the following would also be an acceptable cast
+  // document.getElementById('root')!
+);
+registerServiceWorker();
